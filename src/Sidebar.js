4c9815939c10e3231f5f86547b777a4d9f63c6bb
80050863+sentheta@users.noreply.github.com <80050863+sentheta@users.noreply.github.com>
2025-01-02 14:14:39 +0800

    populate sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
+// import { Logo } from "../public/logo.png"
 
 function Sidebar(){
-    return <div className="sidebar">
-        <h2>Sidebar</h2>
-        <p><Link style={{color:"#abdbe3"}} to="/home">Home</Link></p>
-        <p><Link style={{color:"#abdbe3"}} to="/about">About</Link></p>
-        <p><Link style={{color:"#abdbe3"}} to="/contact">Contact</Link></p>
+    return <div className="sidebar" style={{position:"relative"}}>
+        <img style={{width:"100%", objectFit:"contain"}} src="/logo.png" alt="logo"/>
 
-        <div style={{position:"absolute", bottom:"5vh"}}>
-            Bottom text
-        </div>
+        <h1>AIML Club</h1>
+        <h3><Link style={{color:"#abdbe3"}} to="/home">Home</Link></h3>
+        <h3><Link style={{color:"#abdbe3"}} to="/about">About</Link></h3>
+        {/* <h3><Link style={{color:"#abdbe3"}} to="/contact">Contact</Link></h3> */}
+
+        {/* <div style={{position:"absolute", width:"auto", bottom:"2vh", wordWrap:"break-word"}}>
+            <p>Contact us:</p>
+            <p>dfghjksfduygsdhsjfij</p>
+            <p>+65 1234 5678</p>
+            <p>abcd@efg.hij</p>
+        </div> */}
     </div>
 }
 
